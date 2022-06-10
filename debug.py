5d5e53d5c7c62af334840c1f42ba83dcef8191d7
jaywor1 <misa.javor@gmail.com>
2022-06-10 23:48:18 +0200

    init

---
+++ debug.py
@@ -0,0 +1,8 @@
+def print_ok(msg):
+    print("[   \033[92mO.K\033[0m   ] " + msg)
+
+def print_error(msg):
+    print("[  \033[91mERROR\033[0m  ] " + msg)    
+
+def print_log(msg):
+    print("[   \033[33mLOG\033[0m   ] " + msg)
