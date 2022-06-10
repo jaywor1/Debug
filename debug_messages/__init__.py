f7b9b00a741ff27c60e713dec6b69fca341cebe1
jaywor1 <misa.javor@gmail.com>
2022-06-11 00:56:24 +0200

    Packaging

---
+++ debug_messages/__init__.py
@@ -0,0 +1,1 @@
+from .debug_messages import Debug
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='debug-messages',
+    version='0.1',
+    author='jaywor1',
+    url='https://github.com/jaywor1/Debug',
+    packages=['debug_messages'],
+    
+)
