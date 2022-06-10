f52a32c87b1f70aa979c489e15ba281ee36d7ed4
jaywor1 <misa.javor@gmail.com>
2022-06-11 00:31:45 +0200

    Added fstrings

--- debug.py
+++ debug.py
@@ -4,29 +4,29 @@ class Debug:
     def ok(*args):
         # If there is more than one argument function will assume that the first argument is action (makes it bold)
         if(len(args) == 1):
-            print("[   \033[92mO.K\033[0m   ] " + args[0])
+            print(f"[   \033[92mO.K\033[0m   ] {args[0]}")
         else:
             msg = args[1]
             for i in range(2,len(args)):
                 msg += str(args[i])
-            print("[   \033[92mO.K\033[0m   ] \033[1m" + args[0] + " \033[0m" + msg)
+            print(f"[   \033[92mO.K\033[0m   ] \033[1m{args[0]} \033[0m{msg}")
 
     def error(*args):
         # If there is more than one argument function will assume that the first argument is action (makes it bold)
         if(len(args) == 1):
-            print("[  \033[91mERROR\033[0m  ] " + args[0])
+            print(f"[  \033[91mERROR\033[0m  ] {args[0]}")
         else:
             msg = args[1]
             for i in range(2,len(args)):
                 msg += str(args[i])
-            print("[  \033[91mERROR\033[0m  ] \033[1m" + args[0] + " \033[0m" + msg)
+            print(f"[  \033[91mERROR\033[0m  ] \033[1m{args[0]} \033[0m{msg}")
 
     def log(*args):
         # If there is more than one argument function will assume that the first argument is action (makes it bold)
         if(len(args) == 1):
-            print("[   \033[33mLOG\033[0m   ] " + args[0])
+            print(f"[   \033[33mLOG\033[0m   ] {args[0]}")
         else:
             msg = args[1]
             for i in range(2,len(args)):
                 msg += str(args[i])
-            print("[   \033[33mLOG\033[0m   ] \033[1m" + args[0] + " \033[0m" + msg)
+            print(f"[   \033[33mLOG\033[0m   ] \033[1m{args[0]} \033[0m{msg}")
