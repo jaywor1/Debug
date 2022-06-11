8d961978ade5c906ccfd8c67c685fc84709b1606
jaywor1 <103755136+jaywor1@users.noreply.github.com>
2022-06-11 16:29:10 +0200

    init

---
+++ test.py
@@ -0,0 +1,8 @@
+from debug_messages import Debug
+
+Debug.ok("testing ok method")
+Debug.ok("testing","ok method")
+Debug.error("testing error method")
+Debug.error("testing","error method")
+Debug.log("testin log method")
+Debug.log("testing","log method")
